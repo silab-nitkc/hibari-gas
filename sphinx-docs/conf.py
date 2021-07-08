a949769631efd6b53c5bc81483c3e71c6666ee27
ae21mitsumoto <ae21mitsumoto@g.kumamoto-nct.ac.jp>
2021-07-08 15:13:14 +0900

    N/A

--- sphinx-docs/conf.py
+++ sphinx-docs/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, '/home/lobmto/hachi-obfuscator')
+sys.path.insert(0, '../')
 
 
 # -- Project information -----------------------------------------------------
