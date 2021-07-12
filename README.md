df687276b5cd2a704e16ae098954db2f5c114f39
ae21mitsumoto <ae21mitsumoto@g.kumamoto-nct.ac.jp>
2021-07-12 11:10:11 +0900

    Update README.md

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # hachi-obfuscator
+SMTソルバによる命令列生成を用いた，GNU assemblerの難読化ツール．
+
+![](docs/figure.png)
+
+# Requirement
+* Python 3.9 (or higher)
+* z3py
+* lark-python
+
+# Usage
+```bash
+python obfuscator.py target-path [-o path]
+```
+
+# Example
+```bash
+python obfuscator.py target.s -o out.s
+```
--- obfuscator.py
+++ obfuscator.py
@@ -1,105 +1,111 @@
-from libs.SMT.core import IOExample, Values, Model
-from libs.GAS.simulator import simulate
-from libs.GAS.line import Line
-from libs.GAS.parser import Parser
-from libs.GAS.operand import OperandDict
-from libs.static import *
-import re, random, argparse
-from copy import deepcopy
+from libs.GAS import *
+from libs.SMT import *
+from libs.settings import *
+from libs import color
+from typing import Tuple
+import argparse
 
-parser = argparse.ArgumentParser(description="GAS obfuscator.")
-
-parser.add_argument('src', help='Path of input file.')
-parser.add_argument('-o', help='Path of output file.', default='res.s')
-parser.add_argument('-n', help='Maximum number of lines to obfuscate at once.', default=2, type=int)
-parser.add_argument('-l', help='Number of instructions used for obfuscation.', default=5, type=int)
-
-args = parser.parse_args()
-
-with open(args.src, 'r', encoding='utf-8') as f:
-    raw = f.read()
-
-lines = list(map(Line, raw.split('\n')))
-
-def ignore_imm(op_dict: OperandDict) -> OperandDict:
-    res = OperandDict()
-    for i in op_dict.all.keys():
-        if type(i) is int:
-            continue
-        res.add(op_dict.all[i])
-    
-    return res
-
-def obfuscate(lines):
-    op_dict = Line.operand_dict(lines)
-    ioe = []
-    for i in range(3):
-        init, res, sim = simulate(lines, op_dict)
-        vals = [Values([i.val for i in ignore_imm(init).all.values()], start=True)]
-        vals += [Values() for i in range(args.l+1)]
-        vals += [Values([i.val for i in ignore_imm(res).all.values()], end=True)]
-        ioe += [IOExample(vals)]
-    
-    is_reg = [1 if i.is_reg else 0 for i in ignore_imm(init).all.values()]
-    model = Model(ioe, args.l, is_reg, Parser.REC_COUNT)
-
-    if (m:=model.solve()) is None:
-        pass
-
-    op_code = [m.eval(model.op[i]).as_long() for i in range(args.l)]
-    op_list = model.op_list
-    res = ['' for i in range(args.l)]
-
-    for i in range(args.l):
-        op = op_list[i].op_list[op_code[i]]
-        op.eval_as_GAS(m, res, i, lines[0].suffix)
-    
-    res2 = []
-    for r in res:
-        if r == '':
-            continue
-        res2 += [r]
-
-    return '\n'.join(res2).format(*list(ignore_imm(init).all.keys()) + [r'R{}+{}*8(%rip)'.format(Parser.REC_COUNT, i) for i in range(16)])
-
-def check(lines1: list[Line], lines2: list[Line]):
-    for i in range(1000):
+def check(lines1: list[Line], lines2: list[Line]) -> bool:
+    for i in range(TEST_N):
         op1 = Line.operand_dict(lines1)
         op2 = Line.operand_dict(lines2)
-        if i == 0:
-            for op in op2.all.values():
-                op.val = op.val if op.is_imm else 0
         OperandDict.extend(op1, op2)
 
-        init1, res1, sim1 = simulate(lines1, op1)
-        import code
-        # code.InteractiveConsole(locals()).interact()
-        init2, res2, sim2 = simulate(lines2, op2)
-        if not ignore_imm(res1).same_as(res2):
+        start1, end1, flag1 = simulate(lines1, op1)
+        start2, end2, flag2 = simulate(lines2, op2)
+        if not end1.ignore_imm().same_as(end2.ignore_imm()):
             return False
-    
+
     return True
 
-out = ".data\nR{}: .space 160\n".format(Parser.REC_COUNT)
-for line_idx, line in enumerate(lines):
-    print("\033[33m[Pending]\033[0m\t" + line.raw)
-    print("\033[1A", end='')
-    if line.op and line.op not in ['jz', 'jnz']:
-        done = False
-        for i in range(10):
-            res = obfuscate([line])
-            res_lines = list(map(Line, res.split('\n')))
-            if check([line], res_lines):
-                out += res + '\n'
-                print("\033[32m[ {}/{} ]\t\033[31m".format(line_idx+1, len(lines)) + line.raw + "\033[0m")
-                for l in res_lines:
-                    print("\t\033[32m\t" + l.raw + "\033[0m")
-                done = True
-                break
-        if done:
-            continue
-    out += line.raw + '\n'
-    print("\033[32m[ {}/{} ] \033[0m\t".format(line_idx+1, len(lines)) + line.raw)
+def obfuscate(lines: list[Line], OP_LEN: int, retry: int = 0) -> Tuple[list[Line], bool]:
+    """難読化関数
 
-with open(args.o, 'w', encoding='utf-8') as f:
-    f.write(out)
+    Lineインスタンスのリストを受け取り，難読化したLineインスタンスのリストを返す．
+    難読化に失敗した場合，受け取ったリストをそのまま返す．
+
+    Args:
+        line (Line[]): 難読化対象のGAS
+        OP_LEN (int): 命令列の長さ
+        ttl (int, optional): 難読化を試行する最大回数
+    """
+
+    if retry >= RETRY_MAX:
+        return lines, False
+
+    # 入出力例
+    ioexamples: list[IOExample] = []
+
+    for i in range(IOExample_N):
+        start, end, _ = simulate(lines)
+        start = start.ignore_imm()
+        end   = end.ignore_imm()
+        values = [Values(start.get_values(), start=True)]
+        values += [Values() for i in range(OP_LEN)]
+        values += [Values(end.get_values(), end=True)]
+        ioexamples += [IOExample(values)]
+        
+    is_reg = [op.is_reg for op in start.ignore_imm().all.values()]
+    model = Model(ioexamples, OP_LEN, is_reg)
+
+    if (m:=model.solve()) is None:
+        return lines, False
+    
+    res = merge(model, lines)
+
+    # 入出力テスト
+    if not check(lines, res):
+        print(f'{color.yellow}[Retrying({retry+1})]{color.red}\t{lines[0].raw}{color.reset}')
+        for l in lines[1:]:
+            print(f'{color.red}\t-\t{l.raw}{color.reset}')
+
+        for l in res:
+            print(f'{color.green}\t+\t{l.raw}{color.reset}')
+        
+        color.back_to(len(lines) + len(res))
+        # retry
+        return obfuscate(lines, OP_LEN, retry+1)
+    
+    return res, True
+
+def main():
+    parser = argparse.ArgumentParser(description='GAS obfuscator.')
+    parser.add_argument('src', help='Path of input file.')
+    parser.add_argument('-o', help='Path of output file.', default='res.s')
+    parser.add_argument('-n', help='Maximum number of lines to obfuscate at once.', default=2, type=int)
+    parser.add_argument('-l', help='Number of instructions used for obfuscation.', default=5, type=int)
+
+    args = parser.parse_args()
+
+    with open(args.src, 'r', encoding='utf-8') as f:
+        raw = f.read()
+    
+    lines: list[Line] = list(map(Line, raw.split('\n')))
+    res  : list[Line] = []
+
+    for i, line in enumerate(lines):
+        print(f'{color.yellow}[Pending]{color.reset}\t{line.raw}')
+        color.back_to(1)
+
+        if line.op not in TARGET_OP:
+            res += [line]
+            print(f'[ {i+1}/{len(lines)} ]\t{line.raw}')
+            continue
+        
+        obfuscated_lines, succeded = obfuscate([line], args.l)
+        res += obfuscated_lines
+        
+        if succeded:
+            print(f'{color.yellow}[Obfuscated]{color.red}\t{line.raw}{color.reset}')
+            for l in obfuscated_lines:
+                print(f'{color.green}\t+\t{l.raw}{color.reset}')
+        else:
+            print(f'{color.red}[Skiped]{color.reset}\t{line.raw}')
+    
+    res = insert_data(res)
+    
+    with open(args.o, 'w', encoding='utf-8') as f:
+        f.write('\n'.join([l.raw for l in res]) + '\n')
+
+if __name__ == "__main__":
+    main()
